b509634983e9e871ce93790830fea3ef8c3e4e10
TylerT34 <47005888+TylerT34@users.noreply.github.com>
2019-01-24 14:10:22 -0500

    Started character classes

---
+++ ProjectDCS/Assets/CharacterClasses.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum ClassType {
+	TANK,
+	MEDIC,
+	SNIPER,
+	LIGHT
+}
+
+public class CharacterClasses : MonoBehaviour
+{
+	public ClassType classType;
+
+    void Start()
+    {
+        
+    }
+		
+    void Update()
+    {
+        
+    }
+}
