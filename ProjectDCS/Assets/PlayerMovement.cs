cce29fb3fccbb148ccda5dfa0725c2de7bb43f31
Jacob S <bloggingpenguin@gmail.com>
2019-01-25 13:06:47 -0500

    Added PlayerMovement Script and player prefab
    Player prefab has a PlayerMovement script attached to it, allowing the player to use WSAD or the arrow keys to move the player. Currently, jumping, collision, and mouse look isn't implemented yet.

---
+++ ProjectDCS/Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed = 7.0f;
+	public float lookSpeed = 5.0f;
+
+	void Start() {
+		Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update() {
+		float translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+		float strafe = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+		float lookX = Input.GetAxis("Mouse X") * lookSpeed * Time.deltaTime;
+		float lookY = Input.GetAxis("Mouse Y") * lookSpeed * Time.deltaTime;
+
+		transform.Translate(strafe, 0, translation);
+		if(Input.GetKeyDown("escape")) {
+			Cursor.lockState = CursorLockMode.None;
+		}
+    }
+}
