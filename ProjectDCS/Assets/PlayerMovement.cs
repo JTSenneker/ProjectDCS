349c30d492ee27e3bea88b2b0174789616178bf3
Jacob S <bloggingpenguin@gmail.com>
2019-01-25 13:58:42 -0500

    Implemented Mouse Look
    Implemented mouse look, but it also enables the player to "fly" - an unintentional side effect. Either way, I found out how to implement mouse look.

--- ProjectDCS/Assets/PlayerMovement.cs
+++ ProjectDCS/Assets/PlayerMovement.cs
@@ -1,11 +1,19 @@
-﻿using System.Collections;
+﻿//Apparently noclip is enabled with camera look movement.
+//Gonna fix that, maybe add as a cheat code later? -JS
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
 	public float speed = 7.0f;
-	public float lookSpeed = 5.0f;
+	public float lookSpeedH = 2.0f;
+	public float lookSpeedV = 2.0f;
+	public float yaw = 0.0f;
+	public float pitch = 0.0f;
+
+
 
 	void Start() {
 		Cursor.lockState = CursorLockMode.Locked;
@@ -13,11 +21,15 @@ public class PlayerMovement : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
+		//Player movement
 		float translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
 		float strafe = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
 
-		float lookX = Input.GetAxis("Mouse X") * lookSpeed * Time.deltaTime;
-		float lookY = Input.GetAxis("Mouse Y") * lookSpeed * Time.deltaTime;
+		//Mouse movement
+		yaw += lookSpeedH * Input.GetAxis("Mouse X");
+		pitch -= lookSpeedV * Input.GetAxis("Mouse Y");
+
+		transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
 
 		transform.Translate(strafe, 0, translation);
 		if(Input.GetKeyDown("escape")) {
