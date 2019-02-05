7b6a253d95ff707755765a9a91129acde3b33dc2
Jacob S <bloggingpenguin@gmail.com>
2019-02-05 13:58:55 -0500

    Added gun script, Target script
    Gun "Model" can be changed, as of right now it's just a simple rectangle/cube. We can change the model to something the Graphics students make later. The gun script handles the raycasting, the Target script has to be in anything we want to take damage (Players, Enemy AI, etc). The damage and range of the gun can be changed in the editor, and the health of the object we want to hit can be changed in the editor as well. (Matt wanted me to do the raycasting because he had other stuff to do).

---
+++ ProjectDCS/Assets/Scripts/Gun.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+	public float damage = 10f; //how much damage the gun will do
+	public float range = 100f; //how far the ray will go before not registering a hit
+	public Camera fpsCam;
+	// Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+		if(Input.GetButtonDown("Fire1")) {
+			Shoot();
+		}   
+    }
+
+	void Shoot() {
+		RaycastHit hit;
+		//the line below this comment will return true if the ray hit something and it's in range, and false if it doesn't.
+		if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)) {
+			//log it to the console if we hit something
+			Debug.Log(hit.transform.name);
+			//get the Target component for the object that we hit.
+			Target target = hit.transform.GetComponent<Target>();
+
+			//check if there's a target component...
+			if(target != null) {
+				//if there is, allow the object/enemy/whatever we're shooting at to take damage
+				target.TakeDamage(damage);
+			}
+		}
+	}
+}
---
+++ ProjectDCS/Assets/Scripts/Target.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour {
+	public float health = 50f;
+
+	public void TakeDamage(float damageAmount) {
+		health -= damageAmount;
+		if(health <= 0) {
+			Die();
+		}
+	}
+
+	void Die() {
+		gameObject.SetActive(false);
+	}
+}
