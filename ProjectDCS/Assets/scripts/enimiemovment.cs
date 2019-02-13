a27ad83d8797db7d2dd68469b1864a8086e261c5
hunterbessey <47005826+hunterbessey@users.noreply.github.com>
2019-02-13 13:24:18 -0500

    enemy
    made the enemy move to the player

---
+++ ProjectDCS/Assets/scripts/enimiemovment.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class enimiemovment : MonoBehaviour
+{
+    NavMeshAgent agent;
+   public Transform human;
+   
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+       
+        
+    }
+
+    void Update()
+    {
+
+        agent.SetDestination(human.position);
+    }
+
+
+}
