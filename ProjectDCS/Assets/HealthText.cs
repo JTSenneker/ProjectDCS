f5bee0dab3fdb80dd463894bbb40b0f0b65418f8
TylerT34 <47005888+TylerT34@users.noreply.github.com>
2019-02-13 13:58:36 -0500

    worked on GUI
    started doing the GUI for Health and Ammo.

---
+++ ProjectDCS/Assets/HealthText.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthText : MonoBehaviour
+{
+    public Text healthText;
+    int health = 0;
+
+    private float nextActionTime = 0.0f;
+    public float period = 0.1f;
+
+    void Start()
+    {
+        SetHeathText();
+    }
+
+
+    void Update()
+    {
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += period;
+            
+        }
+    }
+    void SetHealthText()
+    {
+        healthText.text = "Health: " + health.ToString();
+    }
+}
