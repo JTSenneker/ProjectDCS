bea483e8ed39128551e10219730d7e3eabc442cd
hunterbessey <47005826+hunterbessey@users.noreply.github.com>
2019-02-14 18:51:19 -0500

    enemy
    added enemy movement
    added enemy health
    added enemy attack

---
+++ ProjectDCS/Assets/enemyattack.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class enemyattack : MonoBehaviour
+{
+    public float maxditcance;
+    public float cooldowntime;
+    public playerhealth ph;
+
+    private Transform mytransform;
+    public Transform target;
+     Animator controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        mytransform = transform;
+        maxditcance = 3;
+        cooldowntime = 1;
+        controller = this.gameObject.GetComponent<Animator>();
+
+        ph = (playerhealth)target.GetComponent(typeof(playerhealth));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, mytransform.position);
+        if (distance < maxditcance)
+        {
+            attack();
+            controller.Play("attack");
+            
+        }
+        if (cooldowntime > 0)
+        {
+            cooldowntime = cooldowntime * Time.deltaTime;
+        }
+    }
+    void attack()
+    {
+        if (cooldowntime == 0)
+        {
+            ph.changehealth(-10);
+            cooldowntime = 1;
+        }
+
+    }
+}
---
+++ ProjectDCS/Assets/health.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+
+
+public class health : MonoBehaviour
+{
+    public float h = 100f;
+    
+   public Animator final;
+    
+
+    private void Start()
+    {
+        final = this.gameObject.GetComponent<Animator>();
+       
+
+    }
+
+    public void takedamage (float amount)
+    {
+        h -= amount;
+        
+        if (h <= 0f)
+        {
+            die();
+        }
+    }
+    void die()
+    {
+        final.Play("die");
+        Destroy(gameObject);
+        
+    }
+
+    
+
+
+
+
+
+
+}
