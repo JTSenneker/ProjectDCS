841d3edf59d94971c893d0e9a6c1a27093bbf7c4
Nitrate <thesiberiantigercat@gmail.com>
2019-02-26 13:15:04 -0500

    Added pickups

---
+++ ProjectDCS/Assets/Scripts/SpeedCooldown.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedCooldown : MonoBehaviour
+{
+    public Text speedCool;
+    private PlayerMovement myPlayer = new PlayerMovement();
+    int speedCoolDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdatePerSec", 0, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdatePerSec() {
+        speedCoolDown = myPlayer.speedCooldown;
+        speedCool.text = $"S: {speedCoolDown}";
+    }
+}
