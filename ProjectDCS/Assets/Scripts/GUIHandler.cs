21085edbd9cc43554e103d4378ae3480e127fd50
TylerT34 <47005888+TylerT34@users.noreply.github.com>
2019-03-12 14:05:37 -0400

    displayed the UI element

---
+++ ProjectDCS/Assets/Scripts/GUIHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIHandler : MonoBehaviour
+{
+
+    public Image healthBar;
+    public Image armorBar;
+    public Image abilityIcon;
+    public Image speedIcon;
+    public Image powerIcon;
+
+    public Text ammoText;
+    public GameObject weapon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
