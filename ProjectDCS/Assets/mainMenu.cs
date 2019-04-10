8d751e127fe951506a54372b6ea3ef1db7c1eaf4
Jacob Seadorf <bloggingpenguin@gmail.com>
2019-04-10 13:27:10 -0400

    Added Menu scene
    Added main menu and options menu, options menu has a volume slider and a back button, main menu has a play button, an options button, and quit button. However there's an error: "ArgumentException: Input Axis Horizontal is not setup."

---
+++ ProjectDCS/Assets/mainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour {
+    public void playGame() {
+        //load the next scene in the queue
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void quitGame() {
+
+        Application.Quit();
+        Debug.Log("Game Quit");
+    }
+}
