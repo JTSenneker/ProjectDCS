2a0c9f9d4cc6ac412cf9fa7a04178decfd316c14
Joe <jtsenneker@gmail.com>
2019-04-17 12:03:54 -0400

    Laid out UI and started the script

---
+++ ProjectDCS/Assets/Scripts/CharacterSelectController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CharacterSelectController : MonoBehaviour
+{
+    public TextMeshProUGUI classNameText;
+    public RawImage characterPortrait;
+
+    public RenderTexture[] characterRenderTextures;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
