1187212ecb695e9ed03d4f95af5fba0849388d38
hunterbessey <47005826+hunterbessey@users.noreply.github.com>
2019-04-17 13:47:32 -0400

    Added Scripts.
    Not completed.

---
+++ ProjectDCS/Assets/Scripts/opendoor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class opendoor : MonoBehaviour
+{
+    bool interactive;
+   public void Open()
+   {
+        Destroy(gameObject);
+   }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        interactive = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        interactive = false;
+    }
+}
