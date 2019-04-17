9c73f28a041b5387c111c360d6ba7cf00f66968b
Jacob Seadorf <bloggingpenguin@gmail.com>
2019-04-17 12:41:43 -0400

    Added Credits, Menus, Button functionality
    Added people in credits, button functionality, etc.

---
+++ ProjectDCS/Assets/OptionsMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour {
+    public AudioMixer audioMixer;
+
+    public void setVolume(float volume) {
+        Debug.Log(volume);
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+    // 0 = very low, 5 = ultra
+    public void setQuality(int qualityIndex) {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+   
+}
