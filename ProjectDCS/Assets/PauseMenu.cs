07b6a876e246099376205777ed2c2544ac0b4d94
Jacob Seadorf <bloggingpenguin@gmail.com>
2019-04-19 13:08:05 -0400

    Added Pause Menu
    Added Pause menu - couldn't test because there's nothing to pause.

---
+++ ProjectDCS/Assets/PauseMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.P)) {
+            if(isPaused) {
+                Resume();
+            } else {
+                Pause();
+            }
+        }    
+    }
+
+    void Resume() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+
+    void Pause() {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        isPaused = true;
+    }
+}
