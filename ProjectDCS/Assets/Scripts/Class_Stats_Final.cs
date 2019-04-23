f622ecc9e4528871b9d60d0de6986d354e416b2f
MatthewDulak <47005792+MatthewDulak@users.noreply.github.com>
2019-04-23 12:13:28 -0400

    Added Water Balloon, Shield abilities.
    Added Water Balloon, Shield abilities.

---
+++ ProjectDCS/Assets/Scripts/Class_Stats_Final.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum sClass { tank, medic, sniper, joat };
+public class Class_Stats_Final : MonoBehaviour {
+    public float maxHealth;
+    public float maxArmor;
+    public float moveSpeed;
+    public float sprintSpeed;
+    public float currentHealth { get; set; }
+    private float currentArmor;
+    public float ADS;
+    public float abilityCooldown;
+
+    public sClass job;
+
+    public GameObject abilityObject;
+    public Transform spawnPoint;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            UseAbility();
+        }
+    }
+
+    void UseAbility()
+    {
+        switch (job)
+        {
+            case sClass.joat:
+                GameObject waterBalloon = Instantiate(abilityObject, spawnPoint.position, Quaternion.identity);
+                waterBalloon.GetComponent<Rigidbody>().AddForceAtPosition(spawnPoint.forward * 500, transform.position);
+                break;
+            case sClass.medic:
+                //develop MEDIC ability
+                break;
+            case sClass.tank:
+                //develop TANK ability
+                break;
+            case sClass.sniper:
+                //develop SNIPER ability
+                break;
+
+            //raycast and timer for lockpick
+        }
+    }
+}
---
+++ ProjectDCS/Assets/Scripts/Grenade.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    //Jack of all Trades Ability
+    public float delay = 3.0f;
+    public float countdown;
+    public GameObject explosion;
+    bool boom = false;
+    public float blastRadius = 5f;
+    public float splash = 700f;
+
+    void Start()
+    {
+        countdown = delay;
+    }
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0 && !boom)
+        {
+            Explode();
+            boom = true;
+        }
+        void Explode()
+        {
+            //Particles
+            Instantiate(explosion, transform.position, transform.rotation);
+            //Move Objects/Damage
+            Collider[] colliders = Physics.OverlapSphere(transform.position, blastRadius);
+            foreach (Collider nearbyObject in colliders)
+            {
+                //Damage
+                Rigidbody RB = nearbyObject.GetComponent<Rigidbody>();
+                if (RB != null)
+                {
+                    RB.AddExplosionForce(splash, transform.position, blastRadius);
+                }
+            }
+            //Remove Grenade
+            Destroy(gameObject);
+        }
+    }
+}
