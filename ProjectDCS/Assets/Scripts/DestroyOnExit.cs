c175fb68f829daa2d003247e98e96df399c76853
TylerT34 <47005888+TylerT34@users.noreply.github.com>
2019-04-23 13:18:53 -0400

    Made unnecessary improvements

---
+++ ProjectDCS/Assets/Scripts/DestroyOnExit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnExit : MonoBehaviour
+{
+    void OntriggerExit (Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
